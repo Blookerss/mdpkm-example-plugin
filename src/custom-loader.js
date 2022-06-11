d295a945135f6fee5adc5516c0c09c9763837b92
Blookers <blookyl305@gmail.com>
2022-06-11 21:38:10 +1000

    Documentation!

--- src/custom-loader.js
+++ src/custom-loader.js
@@ -2,6 +2,7 @@ import Manifest from './manifest';
 import IconImage from './assets/img/icon.svg';
 import BannerImage from './assets/img/banners/custom-loader.svg';
 export default class CustomLoader extends PluginSystem {
+    // Loader type, important!
     type = 'java-modded';
 
     constructor() {
@@ -9,6 +10,8 @@ export default class CustomLoader extends PluginSystem {
     }
 
     async init() {
+        // Registers a new loader via the plugin api.
+        // http://docs.mdpkm.voxelified.com/docs/plugin-api/pluginsystem#addloader
         this.addLoader('custom_example', {
             icon: IconImage,
             type: this.type,
@@ -17,12 +20,17 @@ export default class CustomLoader extends PluginSystem {
     }
 
     async getVersions() {
+        // in a normal plugin, this should make a request to some external server.
         return {
             '1.18.2': ['1.18.2-1.0.0']
         };
     }
 
     async downloadManifest(path, game, version) {
+        // check out how this is set-up here:
+        // https://github.com/Blookerss/mdpkm-site/tree/main/public
+
+        // http://docs.mdpkm.voxelified.com/docs/plugin-api/api#makeRequest
         const manifest = await API.makeRequest(
             `https://mdpkm.voxelified.com/example-plugin/manifest.json`
         );
